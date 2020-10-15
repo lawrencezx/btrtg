ff34004f0760cf5dbfe330886546cce1a9581b2e
zouxu <sendtozouxu@gmail.com>
2020-10-15 17:51:15 +0800

    fix: include insns.h in generator.h
    When we use nasmlib, we need only include generator.h. So all other related header files
    should be included in generator.h.

--- include/generator.h
+++ include/generator.h
@@ -1,6 +1,8 @@
 #ifndef NASM_GENERATOR_H
 #define NASM_GENERATOR_H
 
+#include "insns.h"
+
 void generator_init(bool set_sequence);
 void generator_exit(void);
 uint32_t generate_bin(insn_seed *seed, const char** buf);
