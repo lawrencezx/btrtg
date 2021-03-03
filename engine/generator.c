ee93b91a29f612b43850201e10f79259f5f04773
zouxu <sendtozouxu@gmail.com>
2021-03-03 21:51:24 +0800

    fix: support rep/repne/repe prefix

--- engine/generator.c
+++ engine/generator.c
@@ -44,6 +44,50 @@ static char codestr[256];
 
 static struct tokenval tokval;
 
+static int prefix_slot(int prefix)
+{
+    switch (prefix) {
+    case P_WAIT:
+        return PPS_WAIT;
+    case R_CS:
+    case R_DS:
+    case R_SS:
+    case R_ES:
+    case R_FS:
+    case R_GS:
+        return PPS_SEG;
+    case P_LOCK:
+        return PPS_LOCK;
+    case P_REP:
+    case P_REPE:
+    case P_REPZ:
+    case P_REPNE:
+    case P_REPNZ:
+    case P_XACQUIRE:
+    case P_XRELEASE:
+    case P_BND:
+    case P_NOBND:
+        return PPS_REP;
+    case P_O16:
+    case P_O32:
+    case P_O64:
+    case P_OSP:
+        return PPS_OSIZE;
+    case P_A16:
+    case P_A32:
+    case P_A64:
+    case P_ASP:
+        return PPS_ASIZE;
+    case P_EVEX:
+    case P_VEX3:
+    case P_VEX2:
+        return PPS_VEX;
+    default:
+        nasm_panic("Invalid value %d passed to prefix_slot()", prefix);
+        return -1;
+    }
+}
+
 int64_t switch_segment(int32_t segment)
 {
     location.segment = segment;
@@ -293,6 +337,18 @@ bool one_insn_gen(const insn_seed *seed, insn *result)
     if (!gen_opcode(seed))
         return false;
     i = get_token(&tokval);
+
+    if (i == TOKEN_PREFIX) {
+        int slot = prefix_slot(tokval.t_integer);
+        if (result->prefixes[slot]) {
+           if (result->prefixes[slot] == tokval.t_integer)
+                nasm_warn(WARN_OTHER, "instruction has redundant prefixes");
+           else
+                nasm_nonfatal("instruction has conflicting prefixes");
+        }
+        result->prefixes[slot] = tokval.t_integer;
+        i = get_token(&tokval);
+    }
 
     if (stat_get_opcode() == I_none)
         stat_set_opcode(tokval.t_integer);
