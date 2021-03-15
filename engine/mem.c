52192c5a53f3752d0c3d050313c4c84e6ae84978
Wang Liangpu <wangjingpu17@mails.ucas.ac.cn>
2021-03-15 20:58:04 +0800

    fix: data_size may be 0 after nasm_random32, feat: add tks for SSE, add some template for some SSE insn

--- engine/mem.c
+++ engine/mem.c
@@ -20,14 +20,18 @@ static bool is_sse2(enum opcode opcode)
 void random_mem_addr_from_data(struct random_mem_addr *daddr, enum SIB_MODE mode)
 {
     int32_t base, scale, index, disp;
-    int data_size;
+    int data_size, data_size_buf;
     static int max_mem_size = 128;
 
     base = nasm_random32(SECTION_DATA_NUM);
     scale = (1 << (nasm_random32(3) + 1));
     data_size = stat_get_data_sec()->datasizes[base] - max_mem_size;
     if (is_sse(stat_get_opcode()) || is_sse2(stat_get_opcode())) {
-        data_size = nasm_random32(data_size);
+        printf("data_size1:%d\n",data_size);
+        while((data_size_buf = nasm_random32(data_size)) == 0)
+            ;
+        data_size = data_size_buf;
+        printf("data_size2:%d\n",data_size);
         disp = nasm_random32(data_size)&XMM_MEM_ALIGN_MASK;
         data_size -= disp;
         index = (data_size/scale)&XMM_MEM_ALIGN_MASK;
